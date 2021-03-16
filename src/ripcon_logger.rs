f2c2f0d3a28b8a8c37ed7e836a741ca94b88f4c9
Seimizu Joukan <saimizi@gmail.com>
2021-03-16 18:48:18 +0900

    ripcon_logger: Add group message loging support.

--- src/ripcon_logger.rs
+++ src/ripcon_logger.rs
@@ -1,6 +1,7 @@
 extern crate ipcon_sys;
 use bytes::Bytes;
 use scheduler;
+use std::collections::HashMap;
 use std::env;
 use std::process::exit;
 
@@ -8,33 +9,92 @@ use getopts::Options;
 use ipcon_sys::ipcon::{Ipcon, IpconFlag};
 use ipcon_sys::ipcon_msg::{IpconKevent, IpconMsg, IpconMsgBody, IpconMsgType};
 use ipcon_sys::logger::env_log_init;
-use ipcon_sys::{debug, error, info, warn};
+use ipcon_sys::{debug, error, error_str_result, info, warn, Result};
 
-fn do_user_msg(msg: IpconMsgBody) {
-    let content = String::from_utf8(msg.buf.to_vec()).unwrap_or(String::from("Non text message"));
-    let group = msg.group.unwrap_or(String::from("?"));
-
-    debug!("do_user_msg: msg.msg_type: {:?}", msg.msg_type);
-    match msg.msg_type {
-        IpconMsgType::IpconMsgTypeNormal => {
-            let lines = content.split("\n");
-
-            for l in lines {
-                info!("{}: {}", msg.peer, l);
-            }
-        }
-        IpconMsgType::IpconMsgTypeGroup => {
-            let lines = content.split("\n");
-            for l in lines {
-                info!("{}@{}: {}", group, msg.peer, l);
-            }
-        }
-        _ => (),
-    }
+struct RIpconLogger {
+    ih: Ipcon,
+    lookup: Option<HashMap<String, bool>>,
 }
 
-fn do_kevent_msg(msg: IpconKevent) {
-    info!("{}", msg.to_string());
+impl RIpconLogger {
+    fn new(mut lookup: Option<HashMap<String, bool>>) -> Result<'static, RIpconLogger> {
+        let ih = Ipcon::new(
+            Some("ripcon_logger"),
+            Some(IpconFlag::IPFDisableKeventFilter),
+        )
+        .expect("Failed to create Ipcon handler");
+
+        ih.join_group(Ipcon::IpconKernelName, Ipcon::IpconKernelGroupName)
+            .expect("failed to join ipcon kevent group");
+
+        if let Some(h) = &mut lookup {
+            for pg in h.keys() {
+                let mut group_peer = pg.split("@");
+                let g = group_peer.next().expect(&format!("Invalid group {}", pg));
+                let p = group_peer.next().expect(&format!("Invalid group {}", pg));
+                if let Ok(_) = ih.join_group(&p, &g) {
+                    info!("Joined group {}@{}", g, p);
+                }
+            }
+        }
+
+        Ok(RIpconLogger {
+            ih: ih,
+            lookup: lookup,
+        })
+    }
+
+    fn receive_msg(&self) -> Result<IpconMsg> {
+        self.ih.receive_msg()
+    }
+
+    fn log_user_msg(&self, msg: IpconMsgBody) {
+        let content =
+            String::from_utf8(msg.buf.to_vec()).unwrap_or(String::from("Non text message"));
+        let group = msg.group.unwrap_or(String::from("?"));
+
+        match msg.msg_type {
+            IpconMsgType::IpconMsgTypeNormal => {
+                let lines = content.split("\n");
+
+                for l in lines {
+                    info!("{}: {}", msg.peer, l);
+                }
+            }
+            IpconMsgType::IpconMsgTypeGroup => {
+                let lines = content.split("\n");
+                for l in lines {
+                    info!("{}@{}: {}", group, msg.peer, l);
+                }
+            }
+            _ => (),
+        }
+    }
+
+    fn log_kevent_msg(&self, msg: IpconKevent) {
+        if let Some(l) = &self.lookup {
+            if let Some((p, g)) = msg.group_added() {
+                let k = format!("{}@{}", g, p);
+                if l.contains_key(&k) {
+                    if let Ok(_) = self.ih.join_group(&p, &g) {
+                        info!("Logger joined group {}@{}", g, p);
+                    }
+                }
+            }
+
+            if let Some((p, g)) = msg.group_removed() {
+                let k = format!("{}@{}", g, p);
+                if l.contains_key(&k) {
+                    info!("Logger left group {}@{}", g, p);
+                }
+            }
+        }
+        info!("{}", msg.to_string());
+    }
+
+    fn free(self) {
+        self.ih.free();
+    }
 }
 
 fn main() {
@@ -52,34 +112,29 @@ fn main() {
         exit(1)
     });
 
-    let ih = Ipcon::new(
-        Some("ripcon_logger"),
-        Some(IpconFlag::IPFDisableKeventFilter),
-    )
-    .expect("failed to create ipcon handler");
-
-    ih.join_group(Ipcon::IpconKernelName, Ipcon::IpconKernelGroupName)
-        .expect("failed to join ipcon kevent group");
+    let mut lookup = HashMap::new();
 
     /*-j group@peer1,group@peer2,,,*/
     match matches.opt_str("j") {
         Some(a) => {
             let groups = a.split(",");
             for t in groups {
-                let mut group_peer = t.split("@");
-                let g = group_peer.next().expect(&format!("Invalid group {}", t));
-                let p = group_peer.next().expect(&format!("Invalid group {}", t));
-                info!("Join {}@{}", g, p);
-                if let Err(e) = ih.join_group(&p, &g) {
-                    warn!("failed to join {}@{} : {}", g, p, e);
-                }
+                lookup.insert(t.to_string(), false);
             }
         }
         None => (),
     }
 
+    let rlogger: RIpconLogger;
+
+    if lookup.is_empty() {
+        rlogger = RIpconLogger::new(None).expect("Failed to create Ipcon handler");
+    } else {
+        rlogger = RIpconLogger::new(Some(lookup)).expect("Failed to create Ipcon handler");
+    }
+
     loop {
-        let msg = match ih.receive_msg() {
+        let msg = match rlogger.receive_msg() {
             Ok(m) => m,
             Err(e) => {
                 error!("{}", e);
@@ -89,14 +144,14 @@ fn main() {
 
         match msg {
             IpconMsg::IpconMsgUser(m) => {
-                do_user_msg(m);
+                rlogger.log_user_msg(m);
             }
             IpconMsg::IpconMsgKevent(k) => {
-                do_kevent_msg(k);
+                rlogger.log_kevent_msg(k);
             }
             IpconMsg::IpconMsgInvalid => (),
         }
     }
 
-    ih.free();
+    rlogger.free();
 }
